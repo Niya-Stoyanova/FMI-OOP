74fa8e9bb5afd0439b1ca32bd0da486cc5c99094
Niya-Stoyanova <49075222+Niya-Stoyanova@users.noreply.github.com>
2019-04-01 19:27:31 +0300

    Update Homework_oop1.cpp

--- Homework_oop1.cpp
+++ Homework_oop1.cpp
@@ -17,19 +17,37 @@ public:
 		this->sec = s;
 	}
 
+	Clock()
+	{
+
+	}
+
 	int getHour()
 	{
 		return hour;
+	}
+	void setHour(int value)
+	{
+		hour = value;
 	}
 
 	int getMin()
 	{
 		return min;
 	}
+	void setMin(int value)
+	{
+		min = value;
+	}
 
 	int getSec()
 	{
 		return sec;
+	}
+
+	void setSec(int value)
+	{
+		sec = value;
 	}
 };
 
@@ -37,7 +55,12 @@ void printWatch()
 {
 	for (int i = 0; i < 10;i++)
 	{
-		Clock clock(5, 5, i);
+		
+		Clock clock;
+		clock.setHour(15);
+		clock.setMin(56);
+		clock.setSec(i);
+		clock.setMin(59);
 		cout << setfill('0') << setw(2) << clock.getHour() << ":" << setfill('0') << setw(2) << clock.getMin() << ":" << setfill('0') << setw(2) << clock.getSec()<<endl;
 
 	}
@@ -50,354 +73,3 @@ int main()
 	return 0;
 }
 
-//class Clock
-//{
-//private:
-//	int hour;
-//	int min;
-//	int sec;
-//
-//public:
-//	Clock(int h, int m, int s)
-//	{
-//		this->hour = h;
-//		this->min = m;
-//		this->sec = s;
-//	}
-//
-//	int getHour()
-//	{
-//		return hour;
-//	}
-//
-//	int getMin()
-//	{
-//		return min;
-//	}
-//
-//	int getSec()
-//	{
-//		return sec;
-//	}
-//};
-//
-//int main()
-//{
-//	Clock clock(5, 5, 6);
-//	cout << setfill('0') << setw(2) << clock.getHour() << ":" << setfill('0') << setw(2) << clock.getMin() << ":" << setfill('0') << setw(2) << clock.getSec();
-//
-//	return 0;
-//}
-
-//class Clock
-//{
-//private:
-//	int hour;
-//	int min;
-//	int sec;
-//
-//public:
-//	Clock(int h, int m, int s)
-//	{
-//		this->hour = h;
-//		this->min = m;
-//		this->sec = s;
-//	}
-//
-//	int getHour()
-//	{
-//		return hour;
-//	}
-//
-//	int getMin()
-//	{
-//		return min;
-//	}
-//
-//	int getSec()
-//	{
-//		return sec;
-//	}
-//};
-//
-//int main()
-//{
-//	Clock clock(15, 4, 8);
-//	cout << setfill('0') << setw(2) << clock.getHour() << ":" << setfill('0') << setw(2) << clock.getMin() << ":" << setfill('0') << setw(2) << clock.getSec();
-//
-//	return 0;
-//}
-
-//class Clock
-//{
-//private:
-//	int hour;
-//	int min;
-//	int sec;
-//
-//public:
-//	Clock(int h, int m, int s)
-//	{
-//		this->hour = h;
-//		this->min = m;
-//		this->sec = s;
-//	}
-//
-//	int getHour()
-//	{
-//		return hour;
-//	}
-//
-//	int getMin()
-//	{
-//		return min;
-//	}
-//
-//	int getSec()
-//	{
-//		return sec;
-//	}
-//};
-//
-//int main()
-//{
-//	Clock clock(5, 5, 50);
-//		cout << setfill('0') << setw(2) << clock.getHour() << ":" << setfill('0') << setw(2) << clock.getMin() << ":" << clock.getSec();
-//
-//	return 0;
-//}
-
-//class Clock
-//{
-//private:
-//	int hours;
-//	int min;
-//	int sec;
-//
-//public:
-//	Clock(int h, int m, int s)
-//	{
-//		this->hours = h;
-//		this->min = m;
-//		this->sec = s;
-//	}
-//
-//	int getHour()
-//	{
-//		return hours;
-//	}
-//
-//	int getMin()
-//	{
-//		return min;
-//	}
-//
-//	int getSec()
-//	{
-//		return sec;
-//	}
-//};
-//
-//int main()
-//{
-//	Clock clock(8, 15, 3);
-//	cout << setfill('0') << setw(2) << clock.getHour() << ":" << setfill('0') << setw(2) << clock.getMin() << ":" << setfill('0') << setw(2) << clock.getSec();
-//
-//	return 0;
-//}
-
-//class Clock
-//{
-//private:
-//	int hours;
-//	int min;
-//	int sec;
-//
-//public:
-//	Clock(int h, int m, int s)
-//	{
-//		this->hours = h;
-//		this->min = m;
-//		this->sec = s;
-//	}
-//
-//	int getHours()
-//	{
-//		return hours;
-//	}
-//
-//	int getMin()
-//	{
-//		return min;
-//	}
-//
-//	int getSec()
-//	{
-//		return sec;
-//	}
-//};
-//
-//int main()
-//{
-//	Clock clock(4, 14, 14);
-//	cout << setfill('0') << setw(2) << clock.getHours()<< ":" << setfill('0') << setw(2)<< clock.getMin() <<":"<< setfill('0') << setw(2)  << clock.getSec();
-//	system("pause");
-//	return 0;
-//}
-
-//class Clock
-//{
-//private:
-//	int hours;
-//	int min;
-//	int sec;
-//
-//public:
-//	Clock(int h, int m, int s)
-//	{
-//		this->hours = h;
-//		this->min = m;
-//		this->sec = s;
-//	}
-//
-//	int getHours()
-//	{
-//		return hours;
-//	}
-//
-//	int getMin()
-//	{
-//		return min;
-//	}
-//
-//	int getSec()
-//	{
-//		return sec;
-//	}
-//};
-//
-//int main()
-//{
-//	Clock clock(15, 15, 15);
-//	cout << clock.getHours() << ":" << clock.getMin() << ":" << clock.getSec() << endl;
-//	
-//	return 0;
-//}
-
-
-//class Clock
-//{
-//private:
-//	int hours;
-//	int min;
-//	int sec;
-//
-//public:
-//	Clock(int h, int m, int s)
-//	{
-//		this->hours = h;
-//		this->min = m;
-//		this->sec = s;
-//	}
-//
-//	int getHours()
-//	{
-//		return hours;
-//	}
-//
-//	int getMin()
-//	{
-//		return min;
-//	}
-//
-//	int getSec()
-//	{
-//		return sec;
-//	}
-//};
-//
-//int main()
-//{
-//	Clock clock(17, 50, 15);
-//	cout << clock.getHours() << ":" << clock.getMin() << ":" << clock.getSec() << endl;
-//
-//	return 0;
-//}
-
-
-//class Clock
-//{
-//private:
-//	int hours;
-//	int sec;
-//	int min;
-//public:
-//	Clock(int h, int m, int s)
-//	{
-//		this->hours = h;
-//		this->min = m;
-//		this->sec = s;
-//	}
-//
-//	int getHours()
-//	{
-//		return hours;
-//	}
-//
-//	int getMin()
-//	{
-//		return min;
-//	}
-//
-//	int getSec()
-//	{
-//		return sec;
-//	}
-//};
-//
-//int main()
-//{
-//	Clock clock(16,28,30);
-//	cout << clock.getHours() << ":" << clock.getMin() << ":" << clock.getSec() << endl;
-//
-//	return 0;
-//}
-
-
-//class Clock
-//{
-//private:
-//	int h;
-//	int min;
-//	int sec;
-//
-//public:
-//	Clock(int hours, int min, int sec)
-//	{
-//		this->h = hours;
-//		this->min = min;
-//		this->sec = sec;
-//	}
-//	
-//	int getHours()
-//	{
-//		return h;
-//	}
-//	int getMin()
-//	{
-//		return min;
-//	}
-//	int getSec()
-//	{
-//		return sec;
-//	}
-//};
-//
-//int main()
-//{
-//	Clock clock(16, 8, 30);
-//	cout << clock.getHours() << " " << clock.getMin() << " " << clock.getSec() << endl;
-//
-//	return 0;
-//
-//}
