e81c0f1313172082d214f24d87bd8aabdf05f7f0
Niya-Stoyanova <49075222+Niya-Stoyanova@users.noreply.github.com>
2019-04-04 20:12:40 +0300

    Update Homework_oop1.cpp

--- Homework_oop1.cpp
+++ Homework_oop1.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 
 class Clock
 {
+
+
 private:
 	int hour;
 	int min;
 	int sec;
 
 public:
+	const int HOUR = 11;
+	const int MIN = 59;
+	const int SEC = 59;
+
 	Clock(int h, int m, int s)
 	{
 		this->hour = h;
@@ -53,23 +60,26 @@ public:
 
 void printWatch()
 {
-	for (int i = 0; i < 10;i++)
-	{
-		
-		Clock clock;
-		clock.setHour(15);
-		clock.setMin(56);
-		clock.setSec(i);
-		
-		cout << setfill('0') << setw(2) << clock.getHour() << ":" << setfill('0') << setw(2) << clock.getMin() << ":" << setfill('0') << setw(2) << clock.getSec()<<endl;
 
+	Clock clock;
+	for (int i = 0; i <= clock.HOUR; i++)
+	{
+		clock.setHour(i);
+		for (int min = 0; min <= clock.MIN; min++)
+		{
+			clock.setMin(min);
+			for (int sec = 0; sec <= clock.SEC; sec++)
+			{
+				clock.setSec(sec);
+				cout << setfill('0') << setw(2) << clock.getHour() << ":" << setfill('0') << setw(2) << clock.getMin() << ":" << setfill('0') << setw(2) << clock.getSec() << endl;
+			}
+		}
 	}
-	
 }
 
 int main()
 {
+
 	printWatch();
 	return 0;
 }
-
